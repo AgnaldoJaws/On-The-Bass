df8c4bfdf7b62d5e74442133e55235c984d6ebc8
Agnaldo <agnaldojr.dep.vc>
2015-09-11 18:59:16 -0300

    Correção do layout

--- index.php
+++ index.php
@@ -15,27 +15,33 @@
 
 <body>
 
+	<div id="logo">
+		<h1>On The Bass</h1>
+		
+		<p>F</p>
+		<p>Yuo</p>
+	</div>
+
 	<header id="home">
 		<div id="flip">
 			<h2 id="a">Menu</h2>
-
 			<div id="menu">
-
 				<ul id="ulMenu">
 					<li><a href="#contato" data-scroll>Contato</a></li>
-
 					<li><a href="#suaBanda" data-scroll>Sua Banda</a></li>
-					<li><a href="#">Videos</a></li>
+					<li><a href="#suaBanda" data-scroll>Contribuir</a></li>
+
 					<li><a href="#tecnica" data-scroll>Técnicas</a></li>
 					<li><a href="#teoria" data-scroll>Teoria</a> <!--  <ul id="abc"> 
-			<li><a href="#">Teoria</a></li> 
-			<li><a href="#">Teoria</a></li> 
-			<li><a href="#">Teoria</a></li> 
-			<li><a href="#">Teoria</a></li> 
-		      </ul> --></li>
+					<li><a href="#">Teoria</a></li> 
+					<li><a href="#">Teoria</a></li> 
+					<li><a href="#">Teoria</a></li> 
+					<li><a href="#">Teoria</a></li> 
+				      </ul> --></li>
+					<li><a href="#tecnica" data-scroll>Sobre</a></li>
 					<li><a href="#">Home</a></li>
+
 				</ul>
-				<h1>ON THE BASS</h1>
 			</div>
 		</div>
 
@@ -43,44 +49,55 @@
 			<ul>
 				<li><a href="#" data-scroll>Contribua</a></li>
 				<li><a href="#" data-scroll>Videos</a></li>
-				<li><a href="#" data-scroll>Técnicas</a></li>
+				<li><a href="#tecnica" data-scroll>Técnicas</a></li>
+				<li><a href="#" data-scroll>Sobre</a>				
 				<li><a href="#" data-scroll>Teoria</a>
+				
 			
 			</ul>
 		</div>
 	</header>
+
 	<!-- Final Menu -->
 
-	<div id="imgDeskTop"></div>
+	
+
 	<div id="txt">
-		<h3 id="txt2">Bem vindo ao maior portal sobre contrabaixo.</h3>
+		<h2 id="txt2">O site do baixista moderno!</h2>
+		<h3>
+			<a href="#start" data-scrooll>LET'S GO</a>
+		</h3>
 	</div>
 
-	<div id="btn">
-		<a href="#start" data-scroll><h3>START</h3></a>
-	</div>
 
-	<div id="btn2">
-		<p></p>
-	</div>
 	<!-- Final img3-->
 
 
-	<section id="section">
-	
+	<section>
+
 		<article id="start">
-			<h2>Sobre</h2>
+			<h2>
+				<center>Start</center>
+				
+			</h2>
+			
 			<a href="header" data-scroll> Topo</a>
-			<div id="anuncio"><p>Anuncio</p></div>
+
+			
 		</article>
-		
-		<article id="teoria">
-			<h2>Teoria</h2>
-			<a href="header" data-scroll>topo</a>
-			<div id="anuncio"><p>Anuncio</p></div>
+
+
+		<article id="tecnica">
+			<h2>
+				<center>Tecncia</center>
+			</h2>
+			
+			<a href="header" data-scroll> Topo</a>
+
+			
 		</article>
-		
-		
+
+
 	</section>
 
 
